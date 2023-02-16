0815374ab361d63e6142c6e93d5177b0005df5a4
Achmad Baihaqi <haqiachd@gmail.com>
2023-02-16 09:05:22 +0700

    tugas acara 3 no 3

---
+++ src/com/wsibd/w2/Tugas33.java
@@ -0,0 +1,15 @@
+package com.wsibd.w2;
+
+public class Tugas33 {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = false;
+        int student = 50, classes = 3;
+        double sales_tax;
+        short number1;
+        int 2beOrNot2b3;
+        double lastYear'sPrice;'
+        long class;
+    }
+}
