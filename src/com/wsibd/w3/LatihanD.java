57f9a28d0144d6a1f24ff876232aaa3212f75f17
Achmad Baihaqi <haqiachd@gmail.com>
2023-02-21 21:45:01 +0700

    tugas minggu ke 3 pertemuan ke 1

---
+++ src/com/wsibd/w3/LatihanD.java
@@ -0,0 +1,14 @@
+package com.wsibd.w3;
+
+public class LatihanD {
+
+    public static void main(String[] args) {
+
+        int[] angka= {3, 1, 42, 24, 12};
+
+        for(int x : angka){
+            System.out.print(x + " ");
+        }
+
+    }
+}
